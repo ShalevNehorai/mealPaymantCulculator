12afdf27bdd57b8993370903cff2be476033e348
ShalevNehorai <shalev336n@gmail.com>
2020-03-18 13:55:29 +0200

    change to tab navogation and make the diners page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:meal_payment_culculator/person.dart';
+import 'package:meal_payment_culculator/tabTest.dart';
 
 import 'meal.dart';
 
 void main() => runApp(Home());
+
+final String appName = "meal payment calc"; 
 
 class Home extends StatefulWidget{
   @override
@@ -12,42 +16,119 @@ class Home extends StatefulWidget{
 
 class AppState extends State<Home>{
 
-  int _currentIndex = 0;
+  final tECDinerName = TextEditingController();
 
-  final _tabs = [//the pages
-    Scaffold(body: Center(child: Text('diners'),),floatingActionButton: FloatingActionButton(child: Icon(Icons.person_add),),),
-    Scaffold(body: Center(child: Text('miles'),),floatingActionButton: FloatingActionButton(child: Icon(Icons.fastfood),),),
-  ];
+  final _diners = <Person>[];
+
+  Widget getDinersPage(BuildContext context){//diners page
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topCenter,
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+            child: Text(
+              'diners page',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          TextField(
+            controller: tECDinerName,
+          ),
+          OutlineButton(
+            splashColor: Colors.cyan,
+            child: Icon(
+              Icons.add,
+              color: Colors.grey[600],
+            ),
+            onPressed: () {
+              String name = tECDinerName.text;
+              if (name.isNotEmpty) {
+                setState(() {
+                  _diners.add(new Person(name));
+                  tECDinerName.clear();
+                });
+                for (var per in _diners) {
+                  print('in list ' + per.name);
+                }
+              }
+              else{
+                Fluttertoast.showToast(
+                  msg: 'the name is empty',
+                  fontSize: 25.0,
+                );
+              }
+            },
+          ),
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.person_add),
+        onPressed: () {
+          print('person');
+        },
+      ),
+    );
+  }
+
+  Widget getDinersList(){
+    return _diners.isEmpty? Card() : ListView.builder(
+      itemCount: _diners.length,
+      itemBuilder: (context, i){
+        print('got here');
+        return ListTile(
+          title: Text(_diners[i].name),
+        );
+      }
+    ); 
+  }
+
+  Widget getMealsPage(BuildContext context){//meals page
+    return Scaffold(
+      body: Center(child: Text('meals page'),),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.fastfood),
+        onPressed: () {
+          print('food');
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: _tabs[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Diners'),
+      title: appName,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(appName),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'Diners', icon: Icon(Icons.people),),
+                Tab(text: 'Meals', icon: Icon(Icons.fastfood),),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.fastfood),
-              title: Text('Meals'),
-            ),
-          ],
-          onTap: (index){
-            setState(() {
-              _currentIndex = index;
-            });
-          },
+          ),
+          body: TabBarView(
+            children: [
+              getDinersPage(context),
+              getMealsPage(context),
+            ],
+          ),
         ),
-      ),
+      ), 
     );
+  }
+
+  @override
+  void dispose() {
+    tECDinerName.dispose();
+    super.dispose();
   }
 }
